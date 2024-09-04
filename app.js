739d0ce2392f7e88d9867ec636c829be2678c668
ankurguha2910 <ankur.guha16@gmail.com>
2024-09-05 03:07:48 +0530

    Building development build and product build, etc.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 //creating an single HTML element
 //It creates a React element that is nothing but a javascript object
 /*const element = React.createElement("h1",
@@ -87,8 +90,8 @@ const heading = React.createElement("div",{id: "parent"},
     [
         React.createElement("div",{id: "child1"},
             [
-                React.createElement("h1",{},"I am an h1 tag"),
-                React.createElement("h2",{},"I am an h2 tag")
+                React.createElement("h1",{},"This is React practice course"),
+                React.createElement("h2",{},"Instructor - Akshay Saini")
             ]
         ),
         React.createElement("div",{id: "child2"},
